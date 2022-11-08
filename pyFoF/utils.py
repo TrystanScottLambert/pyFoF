ada40cc34343d453f71e40b4d0ee34d5f2f0b42d
TrystanScottLambert <TrystanScottLambert@gmail.com>
2022-11-08 16:23:21 +0200

    more readable name changes.

--- pyFoF/utils.py
+++ pyFoF/utils.py
@@ -2,23 +2,25 @@
 
 import numpy as np
 
-def calculate_angular_seperation(longitude_a, latitude_a, longitude_b, latitude_b):
+def calculate_angular_seperation(long_a, lat_a, long_b, lat_b):
     """
     Determines the angular separation between two points, or two catalogs.
     The inputs must be in degrees and can be floats or numpy arrays of floats.
+    This is using the same formula as astropy. Under computational formulaes:
+    https://en.wikipedia.org/wiki/Great-circle_distance
     """
 
     faq = (np.pi/180)
-    longitude_a, longitude_b, latitude_a, latitude_b = longitude_a*faq, longitude_b*faq, latitude_a*faq, latitude_b*faq
-    sdlon = np.sin(longitude_b - longitude_a)
-    cdlon = np.cos(longitude_b - longitude_a)
-    sin_latitude_a = np.sin(latitude_a)
-    sin_latitude_b = np.sin(latitude_b)
-    cos_latitude_a = np.cos(latitude_a)
-    cos_latitude_b = np.cos(latitude_b)
+    long_a, long_b, lat_a, lat_b = long_a*faq, long_b*faq, lat_a*faq, lat_b*faq
+    sin_difference_long = np.sin(long_b - long_a)
+    cos_difference_long = np.cos(long_b - long_a)
+    sin_lat_a = np.sin(lat_a)
+    sin_lat_b = np.sin(lat_b)
+    cos_lat_a = np.cos(lat_a)
+    cos_lat_b = np.cos(lat_b)
 
-    num1 = cos_latitude_b * sdlon
-    num2 = cos_latitude_a * sin_latitude_b - sin_latitude_a * cos_latitude_b * cdlon
-    denominator = sin_latitude_a * sin_latitude_b + cos_latitude_a * cos_latitude_b * cdlon
+    num1 = cos_lat_b * sin_difference_long
+    num2 = cos_lat_a * sin_lat_b - sin_lat_a * cos_lat_b * cos_difference_long
+    denominator = sin_lat_a * sin_lat_b + cos_lat_a * cos_lat_b * cos_difference_long
 
     return np.arctan2(np.hypot(num1, num2), denominator)/faq
