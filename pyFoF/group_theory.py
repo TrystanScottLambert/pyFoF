2addf03045512c08b3d470561e168f63f3ad385a
TrystanScottLambert <TrystanScottLambert@gmail.com>
2022-11-28 00:00:40 +0200

    faster group theory and slightly faster fof.

--- pyFoF/group_theory.py
+++ pyFoF/group_theory.py
@@ -4,7 +4,7 @@ from typing import Tuple, List
 import numpy as np
 import networkx as nx
 from tqdm import tqdm
-
+import datetime
 
 def get_tuples(array_group: np.ndarray) -> Tuple[List, List]:
     """All possible connections within the local group we are checking."""
@@ -14,34 +14,22 @@ def get_tuples(array_group: np.ndarray) -> Tuple[List, List]:
         for j in range(len(group_of_interest)-1-i):
             val_x.append(group_of_interest[i])
             val_y.append(group_of_interest[i+j+1])
-    return val_x,val_y
+    return val_x, val_y
 
 def get_edges(results_list: List, n_runs: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Works out edges and weights of edges for the resulting groups."""
     print('Generating Edges:')
     print('\t 1 of 2: Calculating Pairs')
-    _edges_x, _edges_y=[],[]
-    for i in tqdm(range(len(results_list))):
-        tupples = get_tuples(results_list[i])
-        _edges_x += tupples[0]
-        _edges_y += tupples[1]
-    _edges_x, _edges_y = np.array(_edges_x),np.array(_edges_y)
-
-    print('\t 2 of 2: Calculating Weights')
-    weights,edges_x,edges_y=[],[],[]
-    checked = np.zeros(len(_edges_x))
-    for i in tqdm(range(len(_edges_y))):
-        if checked[i]==0:
-            x_val = np.where(_edges_x==_edges_x[i])
-            y_val = np.where(_edges_y==_edges_y[i])
-            val=np.intersect1d(x_val, y_val)
-            checked[val] = 1
-            number_of_instances = float(len(val))
-            percentage_of_instances = number_of_instances/n_runs
-            weights.append(percentage_of_instances)
-            edges_x.append(_edges_x[i])
-            edges_y.append(_edges_y[i])
-    return edges_x, edges_y, weights
+    tuples = [get_tuples(result_list) for result_list in results_list]
+    tuple_left = np.concatenate([tup[0] for tup in tuples]).astype(int)
+    tuple_right = np.concatenate([tup[1] for tup in tuples]).astype(int)
+    tuple_strings = [f'{tuple_left[i]}-{tuple_right[i]}' for i in range(len(tuple_left))]
+    print('\t 2 of 2: Calculating Edges')
+    number_of_istances = np.unique(tuple_strings, return_counts = True)
+    tuples = [tup.split('-') for tup in number_of_istances[0]]
+    edges_x = np.array([int(tup[0]) for tup in tuples])
+    edges_y = np.array([int(tup[1]) for tup in tuples])
+    return edges_x, edges_y, number_of_istances[1] / n_runs
 
 def get_nodes(results_list: List):
     """Finding all the nodes (i.e. galaxies in groups)."""
@@ -64,13 +52,13 @@ def generate_main_graph(results_list: List, n_runs: int) -> nx.Graph:
 
 def get_subgraphs(graph: nx.Graph):
     """finds all the subgraphs in a graph object."""
-    print('Identifying Subgraphs:')
+    #print('Identifying Subgraphs:')
     sub_graphs = list(graph.subgraph(c).copy() for c in nx.connected_components(graph))
     return sub_graphs
 
 def find_proper_groups(sub_graph_list: List[nx.Graph]) -> List[nx.Graph]:
     """Remove any pairings have less than 3 members. (i.e. not groups.)"""
-    print('Applying > 2 condition for groups.')
+    #print('Applying > 2 condition for groups.')
     return [sub_graph for sub_graph in sub_graph_list if len(sub_graph.nodes) > 2]
 
 def get_node_arrays(stable_list):
@@ -79,10 +67,10 @@ def get_node_arrays(stable_list):
 
 def get_edges_arrays(stable_list: List[nx.Graph]) -> np.ndarray[List]:
     """Writing the edges as arrays"""
-    print('Getting Edge Data')
+    #print('Getting Edge Data')
     edges_array=[]
-    for i in tqdm(range(len(stable_list))):
-        local_edges_full = nx.get_edge_attributes(stable_list[i],'weight')
+    for stable_graph in stable_list:
+        local_edges_full = nx.get_edge_attributes(stable_graph,'weight')
         local_edges_only= list(local_edges_full)
         for edge in local_edges_only:
             local_edge_array=[int(edge[0]), int(edge[1]), float(local_edges_full[edge])]
@@ -139,23 +127,16 @@ def sub_groups(graph: nx.Graph) -> List[nx.Graph]:
     subies = get_node_arrays(list(subies))
     return subies
 
-def subgroup_list(graph_list: List[nx.Graph]) -> List[List[nx.Graph]]:
-    """Works out the sub groups for a list of graphs."""
-    return [sub_groups(graph) for graph in graph_list]
-
-
 def stabalize(results_list, cutoff, n_runs):
     """ averages over multiple runs, returning final groups."""
+
     main_graph = generate_main_graph(results_list, n_runs)
     sub_graphs = get_subgraphs(main_graph)
     edge_data = get_edges_arrays(sub_graphs)
-
     cut_main_graph = cut_edges(main_graph, cutoff)
     sub_graphs = get_subgraphs(cut_main_graph)
     stables = find_proper_groups(sub_graphs)
     stable_arrays = get_node_arrays(stables)
+    weights, weights_normed = wc_list(stables)
 
-    weights, weights_normed=wc_list(stables)
-    sub_groupings = subgroup_list(stables)
-
-    return stable_arrays, edge_data, weights, weights_normed, sub_groupings
+    return stable_arrays, edge_data, weights, weights_normed
